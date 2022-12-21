68440e71b0d65256e39e81149ee156dc478920a5
brijeshhroy <brijeshboxfy@gmail.com>
2022-12-21 14:53:37 +0530

    First commit

---
+++ 1.js
@@ -0,0 +1,8 @@
+// 1. Write a Javascript program to find maximum between two numbers.
+const prompt = require('prompt-sync')();
+
+var a = prompt("Enter the number :- ");
+var b = prompt("Enter the number :- ");
+
+
+a>b ? console.log(a , " is greater") : b > a ? console.log(b , "is greater")  : console.log("Both are equal");
